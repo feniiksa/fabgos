cbb36a4f0589d2ab8c2d6f1fe0528dc58411e8b0
OlikQueen <olysoz@mail.ru>
2025-01-21 00:19:59 +0300

    swiper

--- frontend/main.js
+++ frontend/main.js
@@ -1,1 +1,36 @@
 import "./src/scss/style.scss";
+
+import Swiper from 'swiper';
+import { Navigation, Pagination } from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+// import { Autoplay } from 'swiper/modules';
+// import 'swiper/css/autoplay';
+const swiper = new Swiper('.swiper', {
+
+  modules: [Navigation, Pagination],
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev'
+  },
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  // scrollbar: {
+  //   el: '.swiper-scrollbar',
+  // },
+  // watchOverflow: true,
+  // spaceBetween: 100,
+  // centeredSlidesBounds: true,
+  // loop: true,
+  // speed: 400,
+  // allowTouchMove: false,
+  // breakpoints: {
+  //   768: {
+  //     slidesPerView: 1.05,
+  //     slidesOffsetAfter: 40,
+  //   },
+  // }
+});
