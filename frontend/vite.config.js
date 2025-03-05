34d3b32fb56bb9725715e87798f277354fa7265a
OlikQueen <olysoz@mail.ru>
2025-03-06 01:18:10 +0300

    last

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -8,7 +8,6 @@ export default defineConfig({
         main: path.resolve(__dirname, 'index.html'),
         page2: path.resolve(__dirname, 'ui.html'),
         page3: path.resolve(__dirname, 'inner.html'),
-        page4: path.resolve(__dirname, 'home.html'),
       },
     },
   },
